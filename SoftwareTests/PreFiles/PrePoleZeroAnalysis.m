000f8d6dd589e681e71891efb9ab7cb4bb4aa41a
callie <callie@mathworks.com>
2024-10-28 19:30:17 -0500

    trying pre files to skip over checks for plotting in polezeroanalysis.

---
+++ SoftwareTests/PreFiles/PrePoleZeroAnalysis.m
@@ -0,0 +1,9 @@
+%  Pre-run script for PoleZeroAnalysis.mlx
+% ---- Known Issues     -----
+KnownIssuesID = "";
+% ---- Pre-run commands -----
+plotPoles = @(x)avoidPlotPoles(x);
+function avoidPlotPoles(x)
+    disp("Do not run Plot Poles function.")
+end
+ 
