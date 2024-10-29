cca403767e9851892309932e3f614fa950f33bf0
callie <callie@mathworks.com>
2024-10-29 16:56:49 -0500

    Maintenance of Live Modules Update for 2024b+.
    - Updated CI
    - Updated Smoke Tests
    - Plotting hard-coded color updates
    - Project structure updates
    - Added Instructor Solutions
    
    **WORKING VERSION**

--- SoftwareTests/PreFiles/PrePoleZeroAnalysis.m
+++
@@ -1,9 +0,0 @@
-%  Pre-run script for PoleZeroAnalysis.mlx
-% ---- Known Issues     -----
-KnownIssuesID = "";
-% ---- Pre-run commands -----
-set = @(varargin)avoidSet(varargin);
-function avoidSet(varargin)
-    disp("Do not set parameters.")
-end
- 
